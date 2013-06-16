c51cb91cdd5650f272ca07bbb55bca621cb77cfe
Burton <dburt@a8206612b813.ant.amazon.com>
2013-06-15 20:43:29 -0700

    HelloKindle

---
+++ HelloKindleFire/src/com/example/hellokindlefire/HelloKindleFireActivity.java
@@ -0,0 +1,23 @@
+package com.example.hellokindlefire;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelloKindleFireActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_kindle_fire);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.hello_kindle_fire, menu);
+        return true;
+    }
+    
+}
